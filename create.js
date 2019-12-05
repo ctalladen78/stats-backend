28524c159da3ed84d676a8f01142cb0944cda06d
carlo <cakhill88@gmail.com>
2019-12-05 21:52:01 +0000

    working pointer tables

--- create.js
+++ create.js
@@ -8,36 +8,80 @@ export async function main(event, context) {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
 
-//  const params = {
-//    TableName: process.env.tableName,
-//    Item: {
-//      gameId: unique,
-//      player1: data.player1,
-//      faction1: data.faction1,
-//      commander1: data.commander1,
-//      vp1: data.vp1,
-//      player2 : data.player2,
-//      faction2: data.faction2,
-//      commander2: data.commander2,
-//      vp2: data.vp2,
-//      createdAt: Date.now(),
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      gameId: unique,
+      player1: data.player1,
+      faction1: data.faction1,
+      commander1: data.commander1,
+      vp1: data.vp1,
+      player2 : data.player2,
+      faction2: data.faction2,
+      commander2: data.commander2,
+      vp2: data.vp2,
+      createdAt: Date.now(),
 
-//    }
-//  };
+    }
+  };
 
   const params2 = {
     TableName: process.env.tableName2,
     Item: {
       gameId: unique,
-      player1: data.player1,
-      userId: event.requestContext.identity.cognitoIdentityId,
-    }
+      playerId: data.player1,
+      }
+  };
+
+  const params3 = {
+    TableName: process.env.tableName2,
+    Item: {
+      gameId: unique,
+      playerId: data.player2,
+      }
+  };
+
+  const params4 = {
+    TableName: process.env.tableName3,
+    Item: {
+      gameId: unique,
+      factionId: data.faction1,
+      }
+  };
+
+  const params5 = {
+    TableName: process.env.tableName3,
+    Item: {
+      gameId: unique,
+      factionId: data.faction2,
+      }
+  };
+
+  const params6 = {
+    TableName: process.env.tableName4,
+    Item: {
+      gameId: unique,
+      commanderId: data.commander1,
+      }
+  };
+
+  const params7 = {
+    TableName: process.env.tableName4,
+    Item: {
+      gameId: unique,
+      commanderId: data.commander2,
+      }
   };
 
   try {
- //   await dynamoDbLib.call("put", params);
+    await dynamoDbLib.call("put", params);
     await dynamoDbLib.call("put", params2);
-    return success(/*params.Item,*/ params2.Item);
+    await dynamoDbLib.call("put", params3);
+    await dynamoDbLib.call("put", params4);
+    await dynamoDbLib.call("put", params5);
+    await dynamoDbLib.call("put", params6);
+    await dynamoDbLib.call("put", params7);
+    return success(params.Item);
   } catch (e) {
     return failure({ status: false });
   }
