2201ce83c7f8ab251b3fd2839e3246b65b1fd240
carlo <cakhill88@gmail.com>
2019-12-09 20:04:10 +0000

    Working Create API, uuid inside function

--- submit.js
+++ submit.js
@@ -2,9 +2,9 @@ import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
-const unique = uuid.v1();
 
 export async function main(event, context) {
+  const unique = uuid.v1();
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
 
@@ -83,6 +83,6 @@ export async function main(event, context) {
     await dynamoDbLib.call("put", params7);
     return success(params.Item);
   } catch (e) {
-    return failure({ status: false });
+    return failure({ status: e });
   }
 }
