9a4c95024ad779304a19f6aea0ef3786fb1d92b9
carlo <cakhill88@gmail.com>
2020-01-15 23:40:58 +0000

    added faction api

---
+++ findfaction.js
@@ -0,0 +1,23 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event) {
+  console.log(event);
+  const params = {
+    TableName: process.env.factionProfile,
+    KeyConditionExpression: "factionId = :factionId",
+    ExpressionAttributeValues: {
+        ":factionId": event.pathParameters.id
+    },
+  };
+  console.log(params);
+
+  try {
+    const result = await dynamoDbLib.call("query", params);
+    // Return the matching list of items in response body
+    return success(result.Items);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
+}
