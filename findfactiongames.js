f1a97de900dfb20b4b92a303703a345646992579
carlo <cakhill88@gmail.com>
2020-01-15 23:49:15 +0000

    faction game api

---
+++ findfactiongames.js
@@ -0,0 +1,20 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event) {
+  const params = {
+    TableName: process.env.tableFaction,
+    KeyConditionExpression: "factionId = :factionId",
+    ExpressionAttributeValues: {
+      ":factionId": event.pathParameters.id
+    },
+  };
+
+  try {
+    const result = await dynamoDbLib.call("query", params);
+    // Return the matching list of items in response body
+    return success(result.Items);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
