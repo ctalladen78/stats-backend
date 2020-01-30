faeb47a4be032f23a58094db505d8239dba49bf9
carlo <cakhill88@gmail.com>
2020-01-30 22:57:57 +0000

    added faction commanders get

---
+++ factioncommanders.js
@@ -0,0 +1,22 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event) {
+  event = event.pathParameters.id.replace("%20", " ");
+  const params = {
+    TableName: process.env.commanderProfile,
+    KeyConditionExpression: "factionId = :factionId",
+    ExpressionAttributeValues: {
+        ":factionId": event
+    },
+  };
+
+  try {
+    const result = await dynamoDbLib.call("scan", params);
+    // Return the matching list of items in response body
+    return success(result.Items);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
+}
