ed3ef6edf404e4c2d618ed0596d4b7bbb9f79f4b
carlo <cakhill88@gmail.com>
2020-02-01 20:13:16 +0000

    event.pathParamters

--- deleteresult.js
+++ deleteresult.js
@@ -5,7 +5,7 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event) {
   console.log(event);
 
-  const data = JSON.parse(event.body);
+  const data = event.pathParameters;
 
   console.log(data);
 
