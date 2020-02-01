27ffe3e51da9baa54c044e8eafe3a628879b6e2c
carlo <cakhill88@gmail.com>
2020-02-01 22:15:16 +0000

    error correction

--- deleteresult.js
+++ deleteresult.js
@@ -4,7 +4,7 @@ import { success, failure } from "./libs/response-lib";
 
 export async function main(event) {
   const [data, setData] = useState([]);
-  setdata() = event.pathParameters.id;
+  setData() = event.pathParameters.id;
 
   console.log(data);
 
