05536fa0bea9095b9edde60743e392108d3a6d37
carlo <cakhill88@gmail.com>
2020-02-03 21:47:24 +0000

    another console.log

--- deleteresult.js
+++ deleteresult.js
@@ -154,6 +154,7 @@ export async function main(event) {
   try {
     const gameData = await dynamoDbLib.call("get", findGame(data)); //call game data
     console.log(gameData);
+    console.log(gameData.item.player1);
     const player1Profile = await dynamoDbLib.call("get", findPlayerRank(gameData.item.player1)); // call player 1 data from table
     console.log(player1Profile);
     const player2Profile = await dynamoDbLib.call("get", findPlayerRank(gameData.item.player2)); // call player 2 data from table
