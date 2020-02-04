7ced98e9c1feeb3c9a92fecb323119cb79ad0b45
carlo <cakhill88@gmail.com>
2020-02-04 01:55:08 +0000

    change ranking change direction

--- deleteresult.js
+++ deleteresult.js
@@ -126,16 +126,16 @@ export async function main(event) {
 
     console.log(player1Profile.Item.ranking);
     console.log(gameData.Item.ranking);
-    await dynamoDbLib.call("update", updatePlayerRanks(gameData.Item.player1, (player1Profile.Item.ranking + gameData.Item.ranking)));
-    await dynamoDbLib.call("update", updatePlayerRanks(gameData.Item.player2, (player2Profile.Item.ranking - gameData.Item.ranking)));
+    await dynamoDbLib.call("update", updatePlayerRanks(gameData.Item.player1, (player1Profile.Item.ranking - gameData.Item.ranking)));
+    await dynamoDbLib.call("update", updatePlayerRanks(gameData.Item.player2, (player2Profile.Item.ranking + gameData.Item.ranking)));
     console.log(faction1Profile.Item.ranking + (gameData.Item.ranking*0.2));
     if (gameData.Item.faction1 != gameData.Item.faction2){
-      await dynamoDbLib.call("update", updateFactionRanks(gameData.Item.faction1, (faction1Profile.Item.ranking + (gameData.Item.ranking*0.2))));
-      await dynamoDbLib.call("update", updateFactionRanks(gameData.Item.faction2, (faction2Profile.Item.ranking - (gameData.Item.ranking*0.2))));
+      await dynamoDbLib.call("update", updateFactionRanks(gameData.Item.faction1, (faction1Profile.Item.ranking - (gameData.Item.ranking*0.2))));
+      await dynamoDbLib.call("update", updateFactionRanks(gameData.Item.faction2, (faction2Profile.Item.ranking + (gameData.Item.ranking*0.2))));
     }
     if (gameData.Item.commander1 != gameData.Item.commander2){
-      await dynamoDbLib.call("update", updateCommanderRanks(gameData.Item.commander1, gameData.Item.faction1, (commander1Profile.Item.ranking + (gameData.Item.ranking*0.2))));
-      await dynamoDbLib.call("update", updateCommanderRanks(gameData.Item.commander2, gameData.Item.faction2, (commander2Profile.Item.ranking - (gameData.Item.ranking*0.2))));
+      await dynamoDbLib.call("update", updateCommanderRanks(gameData.Item.commander1, gameData.Item.faction1, (commander1Profile.Item.ranking - (gameData.Item.ranking*0.2))));
+      await dynamoDbLib.call("update", updateCommanderRanks(gameData.Item.commander2, gameData.Item.faction2, (commander2Profile.Item.ranking + (gameData.Item.ranking*0.2))));
     }
 
     const params = {
