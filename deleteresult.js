a028f3e62ae67b0104716f336451b412191fab1c
carlo <cakhill88@gmail.com>
2020-02-04 02:02:44 +0000

    working api removed logs

--- deleteresult.js
+++ deleteresult.js
@@ -5,30 +5,24 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event) {
   const data = event.pathParameters.id;
 
-  console.log(data);
-
   const findGame = (gameId) => {
-    console.log(gameId);
     const params = {
       TableName: process.env.tableHistory,
       Key: {
         gameId: gameId
       }
     };
-    console.log(params);
     return params;
   };
 
   //Calls db based upon player Id passed in
   const findPlayerRank = (player) => {
-    console.log(player);
     const params = {
       TableName: process.env.playerProfile,
       Key: {
         playerId: player
       }
     };
-    console.log(params);
     return params;
   };
 
@@ -39,7 +33,6 @@ export async function main(event) {
         factionId: faction
       }
     };
-    console.log(params);
     return params;
   };
 
@@ -51,13 +44,10 @@ export async function main(event) {
         factionId: faction
       }
     };
-    console.log(params);
     return params;
   };
 
    const updatePlayerRanks = (player, ranking) => {
-    console.log(player);
-    console.log(ranking);
     const params = {
       TableName: process.env.playerProfile,
       Key: {
@@ -72,8 +62,6 @@ export async function main(event) {
     return params;
   };
   const updateFactionRanks = (faction, ranking) => {
-    console.log(faction);
-    console.log(ranking);
     const params = {
       TableName: process.env.factionProfile,
       Key: {
@@ -89,9 +77,6 @@ export async function main(event) {
   };
 
   const updateCommanderRanks = (commander, faction, ranking) => {
-    console.log(commander);
-    console.log(faction);
-    console.log(ranking);
     const params = {
       TableName: process.env.commanderProfile,
       Key: {
@@ -109,26 +94,15 @@ export async function main(event) {
 
   try {
     const gameData = await dynamoDbLib.call("get", findGame(data)); //call game data
-    console.log(gameData);
-    console.log(gameData.Item.player1);
     const player1Profile = await dynamoDbLib.call("get", findPlayerRank(gameData.Item.player1)); // call player 1 data from table
-    console.log(player1Profile);
     const player2Profile = await dynamoDbLib.call("get", findPlayerRank(gameData.Item.player2)); // call player 2 data from table
-    console.log(player2Profile);
     const faction1Profile = await dynamoDbLib.call("get", findFactionRank(gameData.Item.faction1));  // call faction 1 data from table
-    console.log(faction1Profile);
     const faction2Profile = await dynamoDbLib.call("get", findFactionRank(gameData.Item.faction2));  // call faction 2 data from table
-    console.log(faction2Profile);
     const commander1Profile = await dynamoDbLib.call("get", findCommanderRank(gameData.Item.commander1, gameData.Item.faction1));  // call faction 1 data from table
-    console.log(commander1Profile);
     const commander2Profile = await dynamoDbLib.call("get", findCommanderRank(gameData.Item.commander2, gameData.Item.faction2));  // call faction 2 data from table
-    console.log(commander2Profile);
 
-    console.log(player1Profile.Item.ranking);
-    console.log(gameData.Item.ranking);
     await dynamoDbLib.call("update", updatePlayerRanks(gameData.Item.player1, (player1Profile.Item.ranking - gameData.Item.ranking)));
     await dynamoDbLib.call("update", updatePlayerRanks(gameData.Item.player2, (player2Profile.Item.ranking + gameData.Item.ranking)));
-    console.log(faction1Profile.Item.ranking + (gameData.Item.ranking*0.2));
     if (gameData.Item.faction1 != gameData.Item.faction2){
       await dynamoDbLib.call("update", updateFactionRanks(gameData.Item.faction1, (faction1Profile.Item.ranking - (gameData.Item.ranking*0.2))));
       await dynamoDbLib.call("update", updateFactionRanks(gameData.Item.faction2, (faction2Profile.Item.ranking + (gameData.Item.ranking*0.2))));
@@ -144,7 +118,6 @@ export async function main(event) {
         gameId: data
       }
     };
-    console.log(params);
 
     const params2 = {
       TableName: process.env.tablePlayer,
@@ -153,7 +126,6 @@ export async function main(event) {
         playerId: player1Profile.Item.playerId,
         }
     };
-    console.log(params2);
 
     const params3 = {
       TableName: process.env.tablePlayer,
@@ -162,7 +134,6 @@ export async function main(event) {
         playerId: player2Profile.Item.playerId,
         }
     };
-    console.log(params3);
 
     const params4 = {
       TableName: process.env.tableFaction,
@@ -171,7 +142,6 @@ export async function main(event) {
         factionId: faction1Profile.Item.factionId,
         }
     };
-    console.log(params4);
 
     const params5 = {
       TableName: process.env.tableFaction,
@@ -180,7 +150,6 @@ export async function main(event) {
         factionId: faction2Profile.Item.factionId,
         }
     };
-    console.log(params5);
 
     const params6 = {
       TableName: process.env.tableCommander,
@@ -189,7 +158,6 @@ export async function main(event) {
         commanderId: commander1Profile.Item.commanderId,
         }
     };
-    console.log(params6);
 
     const params7 = {
       TableName: process.env.tableCommander,
@@ -198,7 +166,6 @@ export async function main(event) {
         commanderId: commander2Profile.Item.commanderId,
         }
     };
-    console.log(params7);
 
     await dynamoDbLib.call("delete", params);
     await dynamoDbLib.call("delete", params2);
