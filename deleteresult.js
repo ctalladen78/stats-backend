1fc12bf6acf5bb59818b1ffd2b10406b84eb1ef3
carlo <cakhill88@gmail.com>
2020-02-04 01:46:19 +0000

    forgotten .Item

--- deleteresult.js
+++ deleteresult.js
@@ -134,8 +134,8 @@ export async function main(event) {
       await dynamoDbLib.call("update", updateFactionRanks(gameData.Item.faction2, (faction2Profile.Item.ranking - (gameData.Item.ranking*0.2))));
     }
     if (gameData.Item.commander1 != gameData.Item.commander2){
-      await dynamoDbLib.call("update", updateCommanderRanks(gameData.Item.commander1, gameData.Item.faction1, (commander1Profile.Item.ranking + (gameData.ranking*0.2))));
-      await dynamoDbLib.call("update", updateCommanderRanks(gameData.Item.commander2, gameData.Item.faction2, (commander2Profile.Item.ranking - (gameData.ranking*0.2))));
+      await dynamoDbLib.call("update", updateCommanderRanks(gameData.Item.commander1, gameData.Item.faction1, (commander1Profile.Item.ranking + (gameData.Item.ranking*0.2))));
+      await dynamoDbLib.call("update", updateCommanderRanks(gameData.Item.commander2, gameData.Item.faction2, (commander2Profile.Item.ranking - (gameData.Item.ranking*0.2))));
     }
 
     const params = {
