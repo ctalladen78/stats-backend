57e08db70ea9301b2956e51a10dd18b92e6a3737
carlo <cakhill88@gmail.com>
2020-02-06 14:54:05 +0000

    create tournament api

---
+++ createtournament.js
@@ -0,0 +1,28 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+import uuid from "uuid";
+
+export async function main(event, context) {
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+  const unique = uuid.v1();
+
+  const params = {
+    TableName: process.env.tournamentInfo,
+    Item: {
+      tournamentId: unique,
+      adminId: event.requestContext.identity.cognitoIdentityId,
+      tournamentName: data.tournamentName,
+      location: data.location,
+      date: data.date,
+      country: data.country,
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
