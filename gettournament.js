254c78cd5670011894ed04a1f367133522affd6d
carlo <cakhill88@gmail.com>
2020-02-06 17:29:51 +0000

    get for tournament

--- gettournament.js
+++ gettournament.js
@@ -3,18 +3,26 @@ import { success, failure } from "./libs/response-lib";
 
 export async function main(event) {
   const params = {
-    TableName: process.env.tableHistory,
-    KeyConditionExpression: "tournamentId = :tournamentId",
-    ExpressionAttributeValues: {
-      ":tournamentId": event.tournamentId
-    },
+    TableName: process.env.tournamentInfo,
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
+    Key: {
+      tournamentId: event.pathParameters.id
+    }
   };
 
+  console.log(params);
+
   try {
-    const result = await dynamoDbLib.call("query", params);
-    // Return the matching list of items in response body
-    return success(result.Items);
+    const result = await dynamoDbLib.call("get", params);
+    if (result.Item) {
+      // Return the retrieved item
+      return success(result.Item);
+    } else {
+      return failure({ status: false, error: "Item not found." });
+    }
   } catch (e) {
-    return failure({ e });
+    return failure({ status: e });
   }
 }
