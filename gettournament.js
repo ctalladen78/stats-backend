68177c55d8537799834e0215945997a5fb5a5c04
carlo <cakhill88@gmail.com>
2020-02-06 17:34:28 +0000

    more logs

--- gettournament.js
+++ gettournament.js
@@ -2,11 +2,9 @@ import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event) {
+    console.log(event);
   const params = {
     TableName: process.env.tournamentInfo,
-    // 'Key' defines the partition key and sort key of the item to be retrieved
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
     Key: {
       tournamentId: event.pathParameters.id
     }
