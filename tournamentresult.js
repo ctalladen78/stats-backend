cf8eb3dc3d6eb870ec9824addd7fa1b5b57b806f
carlo <cakhill88@gmail.com>
2020-02-11 18:45:05 +0000

    update tournament result API

---
+++ tournamentresult.js
@@ -0,0 +1,26 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event) {
+    console.log(event);
+
+    const params = {
+      TableName: process.env.tableTournamentGames,
+      Key: {
+        gameId: event.pathParameters.id,
+      },
+      UpdateExpression: "SET resultSubmitted = :resultSubmitted",
+      ExpressionAttributeValues: {
+          ":resultSubmitted": true,
+        },
+        ReturnValues: "ALL_NEW"
+      };
+
+    try{
+      await dynamoDbLib.call("update", params);
+    return success(true);
+  } catch (e) {
+    console.log(e);
+    return failure(e);
+  }
+}
