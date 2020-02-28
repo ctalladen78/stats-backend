1fc0044ce835b269468e19ab0650d362295af910
Carlo Hill <cakhill88@gmail.com>
2020-02-28 01:12:19 +0000

    remove round undoes bye changes

--- removeround.js
+++ removeround.js
@@ -14,10 +14,28 @@ export async function main(event, context) {
       }
   };
 
-  console.log(params);
+  const params2 = {
+    TableName: process.env.tournamentPlayers,
+    Key: {
+        playerId: data.player1,
+        tournamentId: data.tournamentId,
+    },
+    UpdateExpression: "SET TPs = :tp, SPs = :sp, gamesPlayed = :gamesPlayed, hadBye = :hadBye",
+    ExpressionAttributeValues: {
+        ":tp": (data.player1Info.TPs - 3),
+        ":sp": (data.player1Info.SPs - 4),
+        ":gamesPlayed": (data.player1Info.gamesPlayed - 1),
+        ":hadBye": false
+    },
+};
+console.log(params);
+console.log(params2);
 
   try {
     await dynamoDbLib.call("delete", params);
+    if (data.player2 === null) {
+      await dynamoDbLib.call("update", params2);      
+    }
     return success(data.gameId);
   } catch (e) {
     return failure(e);
