513da9691361af683dbf66656def0cac353f2d02
Carlo Hill <cakhill88@gmail.com>
2020-03-05 13:46:53 +0000

    Delete does 1 call for game info

--- deleteresult.js
+++ deleteresult.js
@@ -15,47 +15,9 @@ export async function main(event) {
     return params;
   };
 
-  //Calls db based upon player Id passed in
-  const findPlayerRank = (player) => {
-    const params = {
-      TableName: process.env.playerProfile,
-      Key: {
-        playerId: player
-      }
-    };
-    return params;
-  };
-
-  const findFactionRank = (faction) => {
-    const params = {
-      TableName: process.env.factionProfile,
-      Key: {
-        factionId: faction
-      }
-    };
-    return params;
-  };
-
-  const findCommanderRank = (commander, faction) => {
-    const params = {
-      TableName: process.env.commanderProfile,
-      Key: {
-        commanderId: commander,
-        factionId: faction
-      }
-    };
-    return params;
-  };
-
   try {
     const gameData = await dynamoDbLib.call("get", findGame(data)); //call game data
     console.log(gameData);
-    const player1Profile = await dynamoDbLib.call("get", findPlayerRank(gameData.Item.player1)); // call player 1 data from table
-    const player2Profile = await dynamoDbLib.call("get", findPlayerRank(gameData.Item.player2)); // call player 2 data from table
-    const faction1Profile = await dynamoDbLib.call("get", findFactionRank(gameData.Item.faction1));  // call faction 1 data from table
-    const faction2Profile = await dynamoDbLib.call("get", findFactionRank(gameData.Item.faction2));  // call faction 2 data from table
-    const commander1Profile = await dynamoDbLib.call("get", findCommanderRank(gameData.Item.commander1, gameData.Item.faction1));  // call faction 1 data from table
-    const commander2Profile = await dynamoDbLib.call("get", findCommanderRank(gameData.Item.commander2, gameData.Item.faction2));  // call faction 2 data from table
 
     const params = {
       TableName: process.env.tableHistory,
@@ -68,7 +30,7 @@ export async function main(event) {
       TableName: process.env.tablePlayer,
       Key: {
         gameId: data,
-        playerId: player1Profile.Item.playerId,
+        playerId: gameData.Item.player1,
         }
     };
 
@@ -76,7 +38,7 @@ export async function main(event) {
       TableName: process.env.tablePlayer,
       Key: {
           gameId: data,
-        playerId: player2Profile.Item.playerId,
+        playerId: gameData.Item.player2,
         }
     };
 
@@ -84,7 +46,7 @@ export async function main(event) {
       TableName: process.env.tableFaction,
       Key: {
           gameId: data,
-        factionId: faction1Profile.Item.factionId,
+        factionId: gameData.Item.faction1,
         }
     };
 
@@ -92,7 +54,7 @@ export async function main(event) {
       TableName: process.env.tableFaction,
       Key: {
           gameId: data,
-        factionId: faction2Profile.Item.factionId,
+        factionId: gameData.Item.faction2,
         }
     };
 
@@ -100,7 +62,7 @@ export async function main(event) {
       TableName: process.env.tableCommander,
       Key: {
           gameId: data,
-        commanderId: commander1Profile.Item.commanderId,
+        commanderId: gameData.Item.commander1,
         }
     };
 
@@ -108,7 +70,7 @@ export async function main(event) {
       TableName: process.env.tableCommander,
       Key: {
           gameId: data,
-        commanderId: commander2Profile.Item.commanderId,
+        commanderId: gameData.Item.commander2,
         }
     };
 
