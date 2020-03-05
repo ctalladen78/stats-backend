ce92dcd41a7278dc5a5502196efed509f26c22d3
Carlo Hill <cakhill88@gmail.com>
2020-03-05 14:30:43 +0000

    verify results

---
+++ verify.js
@@ -0,0 +1,28 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event) {
+    console.log(event);
+
+    const data = JSON.parse(event.body);
+    console.log(data);
+
+    const params = {
+      TableName: process.env.tableHistory,
+      Key: {
+        gameId: data.gameId,
+      },
+      UpdateExpression: "SET auth1 = :auth1",
+      ExpressionAttributeValues: {
+        ":auth1": true,
+      },
+    };
+
+    try{
+      await dynamoDbLib.call("update", params);
+    return success(true);
+  } catch (e) {
+    console.log(e);
+    return failure(e);
+  }
+}
