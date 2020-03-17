211a1cf0d4e5eb846f71b0147a454dcf28bd1f41
Carlo Hill <cakhill88@gmail.com>
2020-03-17 21:10:34 +0000

    add player accepts no club/affilation

--- services/main/submitplayer.js
+++ services/main/submitplayer.js
@@ -9,37 +9,75 @@ export async function main(event, context) {
   const data = JSON.parse(event.body);
   const random = (Math.ceil(Math.random()*100));
 
+
+
   console.log(data);
 
-  const params = {
-    TableName: process.env.tournamentPlayers,
-    Item: {
-      tournamentId: data.tournamentId,
-      playerId: data.playerId,
-      userName: data.userName,
-      firstName: data.firstName,
-      secondName: data.secondName,
-      faction: data.faction,
-      commander1: data.commander1,
-      commander2: data.commander2,
-      club: data.club,
-      startingRank: data.ranking,
-      seed: random,
-      TPs: 0,
-      SPs: 0,
-      VPs: 0,
-      gamesPlayed: 0,
-      pointsDestroyed: 0,
-      hadBye: false
+  if (data.club == "") {
+    const params = {
+      TableName: process.env.tournamentPlayers,
+      Item: {
+        tournamentId: data.tournamentId,
+        playerId: data.playerId,
+        userName: data.userName,
+        firstName: data.firstName,
+        secondName: data.secondName,
+        faction: data.faction,
+        commander1: data.commander1,
+        commander2: data.commander2,
+        club: null,
+        startingRank: data.ranking,
+        seed: random,
+        TPs: 0,
+        SPs: 0,
+        VPs: 0,
+        gamesPlayed: 0,
+        pointsDestroyed: 0,
+        hadBye: false
+      }
+    };
+
+    console.log(params);
+
+    try {
+      await dynamoDbLib.call("put", params);
+      return success(unique);
+    } catch (e) {
+      console.log(e);
+      return failure({ status: e });
     }
-  };
+  } else {
+    const params = {
+      TableName: process.env.tournamentPlayers,
+      Item: {
+        tournamentId: data.tournamentId,
+        playerId: data.playerId,
+        userName: data.userName,
+        firstName: data.firstName,
+        secondName: data.secondName,
+        faction: data.faction,
+        commander1: data.commander1,
+        commander2: data.commander2,
+        club: data.club,
+        startingRank: data.ranking,
+        seed: random,
+        TPs: 0,
+        SPs: 0,
+        VPs: 0,
+        gamesPlayed: 0,
+        pointsDestroyed: 0,
+        hadBye: false
+      }
+    };
 
-  console.log(params);
+    console.log(params);
 
-  try {
-    await dynamoDbLib.call("put", params);
-    return success(unique);
-  } catch (e) {
-    return failure({ status: e });
+    try {
+      await dynamoDbLib.call("put", params);
+      return success(unique);
+    } catch (e) {
+      console.log(e);
+      return failure({ status: e });
+    }
   }
 }
