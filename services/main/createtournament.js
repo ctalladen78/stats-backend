63a7f916c22da04a0f528280e873960804a06df6
Carlo Hill <cakhill88@gmail.com>
2020-04-09 20:16:57 +0100

    create tournament has TTS option

--- services/main/createtournament.js
+++ services/main/createtournament.js
@@ -9,52 +9,101 @@ export async function main(event, context) {
 
   console.log(data);
 
-  if (data.info == "") {
-    const params = {
-      TableName: process.env.tournamentInfo,
-      Item: {
-        tournamentId: unique,
-        adminId: event.requestContext.identity.cognitoIdentityId,
-        tournamentName: data.tournamentName,
-        location: data.location,
-        date: data.date,
-        country: data.country,
-        region: data.region,
+  if (data.tts === true) {
+    if (data.info == "") {
+      const params = {
+        TableName: process.env.tournamentInfo,
+        Item: {
+          tournamentId: unique,
+          adminId: event.requestContext.identity.cognitoIdentityId,
+          tournamentName: data.tournamentName,
+          ttsTournament: true,
+        }
+      };
+
+      console.log(params);
+
+      try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+      } catch (e) {
+        console.log(e);
+        return failure({ status: false });
       }
-    };
+    } else {
+      const params = {
+        TableName: process.env.tournamentInfo,
+        Item: {
+          tournamentId: unique,
+          adminId: event.requestContext.identity.cognitoIdentityId,
+          tournamentName: data.tournamentName,
+          ttsTournament: true,
+          info: data.info,
+        }
+      };
 
-    console.log(params);
+      console.log(params);
 
-    try {
-      await dynamoDbLib.call("put", params);
-      return success(params.Item);
-    } catch (e) {
-      console.log(e);
-      return failure({ status: false });
+      try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+      } catch (e) {
+        console.log(e);
+        return failure({ status: false });
+      }
     }
-  } else {
-    const params = {
-      TableName: process.env.tournamentInfo,
-      Item: {
-        tournamentId: unique,
-        adminId: event.requestContext.identity.cognitoIdentityId,
-        tournamentName: data.tournamentName,
-        location: data.location,
-        date: data.date,
-        country: data.country,
-        region: data.region,
-        info: data.info,
+  }
+
+  else {
+    if (data.info == "") {
+      const params = {
+        TableName: process.env.tournamentInfo,
+        Item: {
+          tournamentId: unique,
+          adminId: event.requestContext.identity.cognitoIdentityId,
+          tournamentName: data.tournamentName,
+          location: data.location,
+          date: data.date,
+          country: data.country,
+          region: data.region,
+          ttsTournament: false,
+        }
+      };
+
+      console.log(params);
+
+      try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+      } catch (e) {
+        console.log(e);
+        return failure({ status: false });
       }
-    };
+    } else {
+      const params = {
+        TableName: process.env.tournamentInfo,
+        Item: {
+          tournamentId: unique,
+          adminId: event.requestContext.identity.cognitoIdentityId,
+          tournamentName: data.tournamentName,
+          location: data.location,
+          date: data.date,
+          country: data.country,
+          region: data.region,
+          info: data.info,
+          ttsTournament: false,
+        }
+      };
 
-    console.log(params);
+      console.log(params);
 
-    try {
-      await dynamoDbLib.call("put", params);
-      return success(params.Item);
-    } catch (e) {
-      console.log(e);
-      return failure({ status: false });
+      try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+      } catch (e) {
+        console.log(e);
+        return failure({ status: false });
+      }
     }
   }
 }
