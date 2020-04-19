59cbf5f3200d939ed48251856f492dae4c688274
Carlo Hill <cakhill88@gmail.com>
2020-04-19 19:18:42 +0100

    separate list lock from tournament lock

---
+++ services/tournament/tournamentlistunlock.js
@@ -0,0 +1,28 @@
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+
+export async function main(event, context) {
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+
+  console.log(data);
+
+  const params = {
+    TableName: process.env.tournamentInfo,
+    Key: {
+      tournamentId: data,
+    },
+    UpdateExpression: "SET listlocked = :listlocked",
+    ExpressionAttributeValues: {
+      ":listlocked": false,
+    },
+  };
+
+  try {
+    await dynamoDbLib.call("update", params);
+    return success(params.Item);
+  } catch (e) {
+    console.log(e);
+    return failure(e);
+  }
+}
