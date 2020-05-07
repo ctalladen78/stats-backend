c2deff43b493b530ec7a98f368c5ea19a5c8805d
Carlo Hill <cakhill88@gmail.com>
2020-05-07 17:48:39 +0100

    nested block?

--- services/main/deletetournamentttsresult.js
+++ services/main/deletetournamentttsresult.js
@@ -95,13 +95,13 @@ export async function main(event) {
 
   try {
     var gameData = await dynamoDbLib.call("get", findGame(data)); //call game data
-    if (gameData.Item.player1!== undefined && gameData.Item.player2 !== undefined) {
+    if (gameData.Item.player1!== undefined && gameData.Item.player2 !== undefined){
       var player1Profile = await dynamoDbLib.call("get", findPlayerRank(gameData.Item.player1)); // call player 1 data from table
       var player2Profile = await dynamoDbLib.call("get", findPlayerRank(gameData.Item.player2)); // call player 2 data from table
     }
     var faction1Profile = await dynamoDbLib.call("get", findFactionRank(gameData.Item.faction1));  // call faction 1 data from table
     var faction2Profile = await dynamoDbLib.call("get", findFactionRank(gameData.Item.faction2));  // call faction 2 data from table
-    if (gameData.Item.commander1 !== undefined && gameData.Item.commander2 !== undefined) {
+    if (gameData.Item.commander1 !== undefined && gameData.Item.commander2 !== undefined){
       var commander1Profile = await dynamoDbLib.call("get", findCommanderRank(gameData.Item.commander1, gameData.Item.faction1));
       var commander2Profile = await dynamoDbLib.call("get", findCommanderRank(gameData.Item.commander2, gameData.Item.faction2));
     }
