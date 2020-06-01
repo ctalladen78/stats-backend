86c0538cefffd1a13552a8e1822daf7dd3ccd225
Carlo Hill <cakhill88@gmail.com>
2020-06-01 01:56:42 +0100

    remove uuid

--- services/tournament/tournamentedit.js
+++ services/tournament/tournamentedit.js
@@ -1,6 +1,5 @@
 import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
-import uuid from "uuid";
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
