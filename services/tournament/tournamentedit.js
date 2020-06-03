255d6bf1fc70ef8c665ee6b66894b69a18243c16
Carlo Hill <cakhill88@gmail.com>
2020-06-03 14:07:36 +0100

    deal with no tournament info

--- services/tournament/tournamentedit.js
+++ services/tournament/tournamentedit.js
@@ -5,33 +5,62 @@ export async function main(event, context) {
   const data = JSON.parse(event.body);
 
   console.log(data);
+  var params;
 
-  const params = {
-    TableName: process.env.tournamentInfo,
-    Key: {
-      tournamentId: data.tournamentId,
-    },
-    UpdateExpression: "SET tournamentName = :tournamentName, #loc = :newLocation, #date = :newDate, #end = :newEnd, allowSignup = :allowSignup, maxPlayers = :maxPlayers, country = :country, #region = :newRegion, info = :info, ttsTournament = :ttsTournament, pairing = :pairing",
-    ExpressionAttributeValues: {
-        ":tournamentName": data.tournamentName,
-        ":newLocation": data.location,
-        ":newDate": data.date,
-        ":newEnd": data.end,
-        ":allowSignup": data.allowSignup,
-        ":maxPlayers": data.maxPlayers,
-        ":country": data.country,
-        ":newRegion": data.region,
-        ":info": data.info,
-        ":ttsTournament": data.tts,
-        ":pairing": data.pairing,
-    },
-    ExpressionAttributeNames: {
-        "#loc": "location",
-        "#date": "date",
-        "#end": "end",
-        "#region": "region",
-    },
-  };
+  if (data.info == "") {
+    params = {
+      TableName: process.env.tournamentInfo,
+      Key: {
+        tournamentId: data.tournamentId,
+      },
+      UpdateExpression: "SET tournamentName = :tournamentName, #loc = :newLocation, #date = :newDate, #end = :newEnd, allowSignup = :allowSignup, maxPlayers = :maxPlayers, country = :country, #region = :newRegion, ttsTournament = :ttsTournament, pairing = :pairing",
+      ExpressionAttributeValues: {
+          ":tournamentName": data.tournamentName,
+          ":newLocation": data.location,
+          ":newDate": data.date,
+          ":newEnd": data.end,
+          ":allowSignup": data.allowSignup,
+          ":maxPlayers": data.maxPlayers,
+          ":country": data.country,
+          ":newRegion": data.region,
+          ":ttsTournament": data.tts,
+          ":pairing": data.pairing,
+      },
+      ExpressionAttributeNames: {
+          "#loc": "location",
+          "#date": "date",
+          "#end": "end",
+          "#region": "region",
+      },
+    };
+  } else {
+    params = {
+      TableName: process.env.tournamentInfo,
+      Key: {
+        tournamentId: data.tournamentId,
+      },
+      UpdateExpression: "SET tournamentName = :tournamentName, #loc = :newLocation, #date = :newDate, #end = :newEnd, allowSignup = :allowSignup, maxPlayers = :maxPlayers, country = :country, #region = :newRegion, info = :info, ttsTournament = :ttsTournament, pairing = :pairing",
+      ExpressionAttributeValues: {
+          ":tournamentName": data.tournamentName,
+          ":newLocation": data.location,
+          ":newDate": data.date,
+          ":newEnd": data.end,
+          ":allowSignup": data.allowSignup,
+          ":maxPlayers": data.maxPlayers,
+          ":country": data.country,
+          ":newRegion": data.region,
+          ":info": data.info,
+          ":ttsTournament": data.tts,
+          ":pairing": data.pairing,
+      },
+      ExpressionAttributeNames: {
+          "#loc": "location",
+          "#date": "date",
+          "#end": "end",
+          "#region": "region",
+      },
+    };
+  }
 
     try {
         await dynamoDbLib.call("update", params);
