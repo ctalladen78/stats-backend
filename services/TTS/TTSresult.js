0c9315a68f3441f00d5509b491221eda2078bef9
Carlo Hill <cakhill88@gmail.com>
2020-06-22 00:32:10 +0100

    TTS export edits

--- services/TTS/TTSresult.js
+++ services/TTS/TTSresult.js
@@ -9,21 +9,21 @@ export async function main(event) {
 
   console.log(data);
 
-    const params = {
-        TableName: process.env.tableHistory,
-        Item: {
-        gameId: unique,
-        vp1: data.vp1,
-        vp2: data.vp2,
-        }
-    };
+  const params = {
+      TableName: process.env.tableHistory,
+      Item: {
+      gameId: unique,
+      vp1: data.vp1,
+      vp2: data.vp2,
+      }
+  };
 
-    console.log(params);
+  console.log(params);
 
-    try {
-      await dynamoDbLib.call("put", params);
-      return success(data.gameId);
-    } catch (e) {
-      return failure({ status: e });
-    }
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(data.gameId);
+  } catch (e) {
+    return failure({ status: e });
+  }
 }
