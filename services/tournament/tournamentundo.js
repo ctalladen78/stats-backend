26887b15692dfd7f124e9073e59d5ae34a9ed092
Carlo Hill <cakhill88@gmail.com>
2020-07-05 15:41:46 +0100

    add resigned to tournament results

--- services/tournament/tournamentundo.js
+++ services/tournament/tournamentundo.js
@@ -13,7 +13,7 @@ export async function main(event) {
         gameId: data.gameId,
         tournamentId: data.tournamentId,
       },
-      UpdateExpression: "SET resultSubmitted = :set, vp1 = :vp1, vp2 = :vp2, ud1 = :ud1, ud2 = :ud2, commander1 = :commander1, commander2 = :commander2, tp1 = :tp1, tp2 = :tp2, sp1 = :sp1, sp2 = :sp2, destroyed1 = :destroyed1, destroyed2 = :destroyed2, auth1 = :auth1, auth2 = :auth2, pointsLeft1 = :pointsLeft1, pointsLeft2 = :pointsLeft2",
+      UpdateExpression: "SET resultSubmitted = :set, vp1 = :vp1, vp2 = :vp2, ud1 = :ud1, ud2 = :ud2, commander1 = :commander1, commander2 = :commander2, tp1 = :tp1, tp2 = :tp2, sp1 = :sp1, sp2 = :sp2, destroyed1 = :destroyed1, destroyed2 = :destroyed2, resigned1 = :resigned1, resigned2 = :resigned2, auth1 = :auth1, auth2 = :auth2, pointsLeft1 = :pointsLeft1, pointsLeft2 = :pointsLeft2",
       ExpressionAttributeValues: {
         ":set": data.set,
         ":vp1": data.vp1,
@@ -28,6 +28,8 @@ export async function main(event) {
         ":sp2": data.sp2,
         ":destroyed1": data.destroyed1,
         ":destroyed2": data.destroyed2,
+        ":resigned1": data.resigned1,
+        ":resigned2": data.resigned2,        
         ":auth1": false,
         ":auth2": false,
         ":pointsLeft1": 0,
