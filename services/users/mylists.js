25663b853dd31445b1654d705cab8399c5d1260f
Carlo Hill <cakhill88@gmail.com>
2020-07-21 15:47:28 +0100

    get lists uses playerId

--- services/users/mylists.js
+++ services/users/mylists.js
@@ -1,12 +1,12 @@
 import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
 
-export async function main(event, context) {
+export async function main(event) {
   const params = {
     TableName: process.env.savedLists,
     KeyConditionExpression: "playerId = :playerId",
     ExpressionAttributeValues: {
-      ":playerId": event.requestContext.identity.cognitoIdentityId
+      ":playerId": event.pathParameters.id
     },
   };
 
