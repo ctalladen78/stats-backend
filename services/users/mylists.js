2bb4b710d560d64f62d46c32f6e75c3556b0c2e8
Carlo Hill <cakhill88@gmail.com>
2020-07-21 15:58:30 +0100

    syntax error

--- services/users/mylists.js
+++ services/users/mylists.js
@@ -2,7 +2,7 @@ import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
 
 export async function main(event) {
-    console.log(event.pathParameters.id)
+    console.log(event.pathParameters.id);
     const params = {
         TableName: process.env.savedLists,
         KeyConditionExpression: "playerId = :playerId",
