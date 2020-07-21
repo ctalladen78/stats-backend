13164bba2dacda7bd716025752c77b0f4c004b62
Carlo Hill <cakhill88@gmail.com>
2020-07-21 15:53:42 +0100

    add console logs

--- services/users/mylists.js
+++ services/users/mylists.js
@@ -2,19 +2,21 @@ import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
 
 export async function main(event) {
-  const params = {
-    TableName: process.env.savedLists,
-    KeyConditionExpression: "playerId = :playerId",
-    ExpressionAttributeValues: {
-      ":playerId": event.pathParameters.id
-    },
-  };
+    console.log(event.pathParameters.id)
+    const params = {
+        TableName: process.env.savedLists,
+        KeyConditionExpression: "playerId = :playerId",
+        ExpressionAttributeValues: {
+        ":playerId": event.pathParameters.id
+        },
+    };
 
-  try {
-    const result = await dynamoDbLib.call("query", params);
-    // Return the matching list of items in response body
-    return success(result.Items);
-  } catch (e) {
-    return failure({ status: false });
-  }
+    try {
+        const result = await dynamoDbLib.call("query", params);
+        // Return the matching list of items in response body
+        return success(result.Items);
+    } catch (e) {
+        console.log(e);
+        return failure({ status: false });
+    }
 }
