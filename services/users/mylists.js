6e24f1ac4f927c7e65be019a26297be8d5f75cb2
Carlo Hill <cakhill88@gmail.com>
2020-07-21 16:24:10 +0100

    log result

--- services/users/mylists.js
+++ services/users/mylists.js
@@ -2,7 +2,6 @@ import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
 
 export async function main(event) {
-    console.log(event.pathParameters.id);
     const params = {
         TableName: process.env.savedLists,
         FilterExpression: "playerId = :playerId",
@@ -13,6 +12,7 @@ export async function main(event) {
 
     try {
         const result = await dynamoDbLib.call("scan", params);
+        console.log(result);
         // Return the matching list of items in response body
         return success(result.Items);
     } catch (e) {
