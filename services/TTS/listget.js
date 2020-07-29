0e1286451e57cd5f59dd6c5e5562d0de03c44045
Carlo Hill <cakhill88@gmail.com>
2020-07-29 01:42:29 +0100

    more console logs

--- services/TTS/listget.js
+++ services/TTS/listget.js
@@ -2,6 +2,8 @@ import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
 
 export async function main(event) {
+  console.log(event);
+  console.log(event.listId);
   const params = {
     TableName: process.env.savedLists,
     Key: {
