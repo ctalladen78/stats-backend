4834c3f085c34dd79dd25408d1ad147585c6a6a6
Carlo Hill <cakhill88@gmail.com>
2020-07-30 16:34:47 +0100

    put Id into object?£

--- services/TTS/listget.js
+++ services/TTS/listget.js
@@ -3,7 +3,7 @@ import { success, failure } from "../../libs/response-lib";
 
 export async function main(event) {
   const listIdWith = event.queryStringParameters.listId;
-  const listId = listIdWith.slice(1, 37);
+  const listId = {listIdWith};
 
   console.log(listId);
 
