b01c3aab4dac2b3e0e206386dccb81d52f71da87
Carlo Hill <cakhill88@gmail.com>
2020-07-30 16:10:45 +0100

    use slice properly

--- services/TTS/listget.js
+++ services/TTS/listget.js
@@ -3,7 +3,7 @@ import { success, failure } from "../../libs/response-lib";
 
 export async function main(event) {
   const listIdWith = event.queryStringParameters.listId;
-  const listId = listIdWith.slice(0, -1).slice(0, 1);
+  const listId = listIdWith.slice(1, 37)
 
   console.log(listId);
 
