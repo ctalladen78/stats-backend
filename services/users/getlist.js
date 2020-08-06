8ab5be523f347a15cb24c728c70b40ff1f501b50
Carlo Hill <cakhill88@gmail.com>
2020-08-06 12:41:35 +0100

    trailing spaces

--- services/users/getlist.js
+++ services/users/getlist.js
@@ -3,7 +3,7 @@ import { success, failure } from "../../libs/response-lib";
 
 export async function main(event) {
     console.log(event);
-    
+
   const params = {
     TableName: process.env.savedLists,
     KeyConditionExpression: "listId = :listId",
