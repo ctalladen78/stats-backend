758b0e6260caaa51f52f2d51ab46732891f79ea8
Carlo Hill <cakhill88@gmail.com>
2020-08-06 12:37:02 +0100

    console logs

--- services/users/getlist.js
+++ services/users/getlist.js
@@ -2,7 +2,8 @@ import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
 
 export async function main(event) {
-
+    console.log(event);
+    
   const params = {
     TableName: process.env.savedLists,
     KeyConditionExpression: "listId = :listId",
@@ -10,6 +11,8 @@ export async function main(event) {
         ":listId": event.pathParameters.id
     },
   };
+
+  console.log(params);
 
   try {
     const result = await dynamoDbLib.call("query", params);
