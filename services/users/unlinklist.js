b5ad8b7868794e1f26ef5153427fa17c39dc2006
Carlo Hill <cakhill88@gmail.com>
2020-08-07 12:52:50 +0100

    use delete instead

--- services/users/unlinklist.js
+++ services/users/unlinklist.js
@@ -2,22 +2,24 @@ import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
 
 export async function main(event) {
+    console.log(event.body);
+    
     const params = {
         TableName: process.env.savedLists,
         Key: {
             listId: event.pathParameters.id,
             playerId: event.requestContext.identity.cognitoIdentityId,
         },
-        UpdateExpression: "REMOVE playerId",
     };
 
     console.log(params);
 
     try {
-        await dynamoDbLib.call("update", params);
+        await dynamoDbLib.call("delete", params);
         return success();
     } catch (e) {
         console.log(e);
         return failure({ status: e });
     }
+
 }
