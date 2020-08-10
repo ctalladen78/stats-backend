0752ceb5c2bd813e92111c9691ed4fd8486c4bb6
Carlo Hill <cakhill88@gmail.com>
2020-08-10 14:53:36 +0100

    wrong js in serverless

--- services/main/addlist.js
+++ services/main/addlist.js
@@ -2,13 +2,10 @@ import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
 
 export async function main(event) {
-    console.log(event);
     const data = JSON.parse(event.body);
     console.log(data);
-    console.log(data.player);
-    console.log(data.list);
 
-    // if (data.player === 1) {
+    if (data.player === 1) {
         console.log("HERE");
         const params = {
         TableName: process.env.tableHistory,
@@ -29,26 +26,26 @@ export async function main(event) {
         console.log(e);
         return failure(e);
         }
-    // }
+    }
 
-    // if (data.player === 2) {
-    //     const params = {
-    //     TableName: process.env.tableHistory,
-    //     Key: {
-    //         gameId: event.pathParameters.id,
-    //     },
-    //     UpdateExpression: "SET list2Location = :list2Location",
-    //     ExpressionAttributeValues: {
-    //         ":list2Location": data.list,
-    //     },
-    //     };
+    if (data.player === 2) {
+        const params = {
+        TableName: process.env.tableHistory,
+        Key: {
+            gameId: event.pathParameters.id,
+        },
+        UpdateExpression: "SET list2Location = :list2Location",
+        ExpressionAttributeValues: {
+            ":list2Location": data.list,
+        },
+        };
 
-    //     try{
-    //     await dynamoDbLib.call("update", params);
-    //     return success(true);
-    //     } catch (e) {
-    //     console.log(e);
-    //     return failure(e);
-    //     }
-    // }
+        try{
+        await dynamoDbLib.call("update", params);
+        return success(true);
+        } catch (e) {
+        console.log(e);
+        return failure(e);
+        }
+    }
 }
