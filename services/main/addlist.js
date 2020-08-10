64d539a88ef3e365a1178a005a8ce437ac48cf3b
Carlo Hill <cakhill88@gmail.com>
2020-08-10 12:24:54 +0100

    add list to result

---
+++ services/main/addlist.js
@@ -0,0 +1,49 @@
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+
+export async function main(event) {
+    const data = JSON.parse(event.body);
+    console.log(data);
+
+    if (data.player === 1) {
+        const params = {
+        TableName: process.env.tableHistory,
+        Key: {
+            gameId: event.pathParameters.id,
+        },
+        UpdateExpression: "SET list1Location = :list1Location",
+        ExpressionAttributeValues: {
+            ":list1Location": data.list,
+        },
+        };
+
+        try{
+        await dynamoDbLib.call("update", params);
+        return success(true);
+        } catch (e) {
+        console.log(e);
+        return failure(e);
+        }
+    }
+
+    if (data.player === 2) {
+        const params = {
+        TableName: process.env.tableHistory,
+        Key: {
+            gameId: event.pathParameters.id,
+        },
+        UpdateExpression: "SET list2Location = :list2Location",
+        ExpressionAttributeValues: {
+            ":list2Location": data.list,
+        },
+        };
+
+        try{
+        await dynamoDbLib.call("update", params);
+        return success(true);
+        } catch (e) {
+        console.log(e);
+        return failure(e);
+        }
+    }
+}
