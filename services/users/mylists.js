ff972d1705186a5ca68d4305dc49d34fde77d4cf
Carlo Hill <cakhill88@gmail.com>
2020-08-12 16:13:42 +0100

    implement get lists loop

--- services/users/mylists.js
+++ services/users/mylists.js
@@ -2,6 +2,8 @@ import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
 
 export async function main(event) {
+var allResults = [];
+
     const params = {
         TableName: process.env.savedLists,
         FilterExpression: "playerId = :playerId",
@@ -10,13 +12,27 @@ export async function main(event) {
         },
     };
 
-    console.log(params);
+    const getAllData = async (params) => { 
+
+        console.log("Querying Table");
+        let data = await dynamoDbLib.call("scan", params);
+    
+        if(data['Items'].length > 0) {
+            allResults = [...allResults, ...data['Items']];
+        }
+    
+        if (data.LastEvaluatedKey) {
+            params.ExclusiveStartKey = data.LastEvaluatedKey;
+            return await getAllData(params);
+        } else {
+            return data;
+        }
+    }
 
     try {
-        const result = await dynamoDbLib.call("scan", params);
-        console.log(result);
-        console.log(result.Items);
-        return success(result.Items);
+        await getAllData(params);
+        console.log("Processing Completed");
+        return success(allResults);
     } catch (e) {
         console.log(e);
         return failure({ status: false });
