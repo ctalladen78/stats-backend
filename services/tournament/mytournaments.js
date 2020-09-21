a7518971dd4629ed31b4c28b868b6f2e2cea657c
Carlo Hill <cakhill88@gmail.com>
2020-09-21 12:44:21 +0100

    scan for my tournaments

---
+++ services/tournament/mytournaments.js
@@ -0,0 +1,22 @@
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+
+export async function main(event) {
+  event = event.pathParameters.id;
+  const params = {
+    TableName: process.env.tournamentPlayers,
+    FilterExpression: "playerId = :playerId",
+    ExpressionAttributeValues: {
+        ":playerId": event
+    },
+  };
+
+  try {
+    const result = await dynamoDbLib.call("scan", params);
+    // Return the matching list of items in response body
+    return success(result.Items);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
+}
