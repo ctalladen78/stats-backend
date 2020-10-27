ee45aee68b6cbccbd15a54f7ce2bc51c71ba28f2
Carlo Hill <cakhill88@gmail.com>
2020-10-27 03:40:40 +0000

    change commanders and faction APIs

---
+++ services/tournament/changefaction.js
@@ -0,0 +1,34 @@
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  console.log(data);
+
+    const params = {
+        TableName: process.env.tournamentPlayers,
+        Key: {
+          playerId: data.playerId,
+          tournamentId: data.tournamentId,
+        },
+        UpdateExpression: "SET faction = :faction, commander1 = :commander1, commander2 = :commander2, list1 = :list1, list2 = :list2, list1Location = :list1Location, list2Location = :list2Location",
+        ExpressionAttributeValues: {
+            ":faction": data.faction,
+            ":commander1": data.commander1,
+            ":commander2": data.commander2,
+            ":list1": data.list1,
+            ":list2": data.list2,
+            ":list1Location": data.list1Location,
+            ":list2Location": data.list2Location,
+        },
+    };
+
+    try {
+      await dynamoDbLib.call("update", params);
+      return success();
+    } catch (e) {
+      console.log(e);
+      return failure({ status: e });
+    }
+  }
