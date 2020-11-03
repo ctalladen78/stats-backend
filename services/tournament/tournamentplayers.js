e5f33068f482f10749590ca3c87ee02a852182a3
Carlo Hill <cakhill88@gmail.com>
2020-11-03 14:30:59 +0000

    get tournament players uses last evaulated key

--- services/tournament/tournamentplayers.js
+++ services/tournament/tournamentplayers.js
@@ -11,12 +11,29 @@ export async function main(event) {
     },
   };
 
+    const getAllData = async (params) => {
+
+      console.log("Querying Table");
+      let data = await dynamoDbLib.call("scan", params);
+
+      if(data['Items'].length > 0) {
+          allResults = [...allResults, ...data['Items']];
+      }
+
+      if (data.LastEvaluatedKey) {
+          params.ExclusiveStartKey = data.LastEvaluatedKey;
+          return await getAllData(params);
+      } else {
+          return data;
+      }
+  };
+
   try {
-    const result = await dynamoDbLib.call("scan", params);
-    // Return the matching list of items in response body
-    return success(result.Items);
+      await getAllData(params);
+      console.log("Processing Completed");
+      return success(allResults);
   } catch (e) {
-    console.log(e);
-    return failure({ status: false });
+      console.log(e);
+      return failure({ status: false });
   }
 }
