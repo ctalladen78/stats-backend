fce225ae21efe2a500cab2a84f242bbb005a8843
Carlo Hill <cakhill88@gmail.com>
2020-12-20 13:31:20 +0000

    scan all players tournaments

--- services/tournament/mytournaments.js
+++ services/tournament/mytournaments.js
@@ -1,22 +1,41 @@
 import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
 
-export async function main(event) {
-  event = event.pathParameters.id;
-  const params = {
-    TableName: process.env.tournamentPlayers,
-    FilterExpression: "playerId = :playerId",
-    ExpressionAttributeValues: {
-        ":playerId": event
-    },
-  };
+export async function main(event, context) {
+    event = event.pathParameters.id;
 
-  try {
-    const result = await dynamoDbLib.call("scan", params);
-    // Return the matching list of items in response body
-    return success(result.Items);
-  } catch (e) {
-    console.log(e);
-    return failure({ status: false });
-  }
+    var allResults = [];
+
+    const params = {
+      TableName: process.env.tournamentPlayers,
+      FilterExpression: "playerId = :playerId",
+      ExpressionAttributeValues: {
+          ":playerId": event
+      },
+    };
+
+    const getAllData = async (params) => {
+
+        console.log("Querying Table");
+        let data = await dynamoDbLib.call("scan", params);
+
+        if(data['Items'].length > 0) {
+            allResults = [...allResults, ...data['Items']];
+        }
+
+        if (data.LastEvaluatedKey) {
+            params.ExclusiveStartKey = data.LastEvaluatedKey;
+            return await getAllData(params);
+        } else {
+            return data;
+        }
+    };
+
+    try {
+        await getAllData(params);
+        console.log("Processing Completed");
+        return success(allResults);
+    } catch (e) {
+        return failure({ e });
+    }
 }
